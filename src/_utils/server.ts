88efa9d52c5005d63c3dc48ccbfdc900c4055b79
Jeison Fox <jeisonfox@gmail.com>
2025-06-15 21:16:34 -0500

    Feature: Update default text.

--- src/_utils/server.ts
+++ src/_utils/server.ts
@@ -11,7 +11,7 @@ export async function getBandsData(): Promise<Band[]> {
   const enrichedBandsData = await Promise.all(
     bandsData.map(async (band) => {
       let bandDescription =
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate ve";
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea";
 
       const detailsPath = path.join(process.cwd(), "src", "_mocks", "details", `${band.id}.json`);
 
